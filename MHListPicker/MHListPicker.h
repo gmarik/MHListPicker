b073d5bebb99d359e3e9242d6940e53d1a2c8c6a
gmarik <gmarik@gmail.com>
2013-06-10 05:54:37 -0500

    group code

---
+++ MHListPicker/MHListPicker.h
@@ -0,0 +1,13 @@
+//
+//  MHListPicker.h
+//  MHListPicker
+//
+//  Created by Maryan Hratson on 2013-06-10.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MHListPicker : NSObject
+
+@end
---
+++ MHListPicker/MHListPicker.m
@@ -0,0 +1,13 @@
+//
+//  MHListPicker.m
+//  MHListPicker
+//
+//  Created by Maryan Hratson on 2013-06-10.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import "MHListPicker.h"
+
+@implementation MHListPicker
+
+@end
